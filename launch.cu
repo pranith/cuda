7a256370a9d21ef73e0c7a525118b36585b6c884
Pranith Kumar <bobby.prani@gmail.com>
2014-06-23 18:48:04 -0400

    working overhead measurement

--- launch.cu
+++ launch.cu
@@ -1,5 +1,6 @@
 #include <cuda.h>
 #include <stdlib.h>
+#include <iostream>
 
 #define THREADS_PER_BLOCK 32
 #define BLOCKS_PER_SM 6
@@ -7,7 +8,7 @@
 #define KB(x) ((x) << 10)
 #define MB(x) ((x) << 20)
 
-__global__ void test_kernel(int maxIdx)
+__global__ void test_kernel(char *data, int maxIdx)
 {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -23,10 +24,26 @@ int main()
 
 	for (int i = 1; i < 256; i++) {
 		int maxIdx = MB(i);
-		data = cudaMallocManaged(MB(i));
+		float time_elapsed, total_time = 0;
+		cudaEvent_t before, after;
+		cudaEventCreate(&before);
+		cudaEventCreate(&after);
+		cudaMallocManaged(&data, MB(i));
+		for (int tries = 0; tries < 5; tries++) {
+			cudaEventRecord(before, 0);
+			test_kernel<<<BLOCKS_PER_SM, THREADS_PER_BLOCK>>>(data, maxIdx);
+			cudaDeviceSynchronize();
+			cudaEventRecord(after, 0);
 
-		test_kernel<<<BLOCKS_PER_SM, THREADS_PER_SM>>>(maxIdx);
-		cudaDeviceSynchronize();
+			cudaEventSynchronize(before);
+			cudaEventSynchronize(after);
+			cudaEventElapsedTime(&time_elapsed, before, after);
+			total_time += time_elapsed;
+		}
+		std::cout << i << "," << total_time/5 << std::endl;
+		cudaFree(data);
 	}
+
+	return 0;
 }
 
