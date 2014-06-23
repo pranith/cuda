b4a39f978744655825f9a878ef9c396e189bb9bf
Pranith Kumar <bobby.prani@gmail.com>
2014-06-23 18:27:26 -0400

    add a new line :-/

--- test.cu
+++ test.cu
@@ -5,6 +5,7 @@ __device__ __managed__ int ret[1000];
 __global__ void AplusB(int a, int b) {
 	ret[threadIdx.x] = a + b + threadIdx.x;
 }
+
 int main() {
 	AplusB<<< 1, 1000 >>>(10, 100);
 	cudaDeviceSynchronize();
